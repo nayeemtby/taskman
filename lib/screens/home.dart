295783b53790357c1daf7d44b76e4b30c1e34526
nayeemtby <nayeemtby@gmail.com>
2021-12-05 23:36:08 +0600

    Finished profile page, added conditional sheet

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:taskman/screens/calender_page.dart';
+import 'package:taskman/screens/components/buttons.dart';
 import 'package:taskman/screens/profile_page.dart';
 import 'package:taskman/screens/project_page.dart';
 import 'components/appbar.dart';
@@ -19,6 +20,12 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _currentIndex = 0;
   late PageController _pageController;
+  final List<Widget?> _sheets = [
+    const AddTaskSheet(),
+    null,
+    null,
+    const AddProjectSheet()
+  ];
   @override
   void initState() {
     super.initState();
@@ -53,7 +60,8 @@ class _HomeScreenState extends State<HomeScreen> {
         unselectedLabelStyle: TxtTheme.caption2,
         unselectedItemColor: swatch.LabelColors.secondary,
         items: const [
-          BottomNavigationBarItem(label: "My Task", icon: Icon(Icons.task)),
+          BottomNavigationBarItem(
+              label: "My Task", icon: Icon(Icons.task_outlined)),
           BottomNavigationBarItem(
               label: "Calendar", icon: Icon(Icons.calendar_today_outlined)),
           BottomNavigationBarItem(
@@ -70,33 +78,44 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       floatingActionButton: Padding(
         padding: const EdgeInsets.only(top: 50),
-        child: Material(
-          elevation: 0,
-          borderRadius: BorderRadius.circular(40),
-          color: swatch.PrimaryColors.blue,
-          child: Builder(builder: (context) {
-            return InkWell(
-                splashFactory: InkRipple.splashFactory,
-                borderRadius: BorderRadius.circular(40),
-                onTap: () {
-                  showModalBottomSheet(
-                      clipBehavior: Clip.hardEdge,
-                      isScrollControlled: true,
-                      shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(8),
-                              topRight: Radius.circular(8))),
-                      context: context,
-                      builder: (context) => const AddTaskSheet());
-                },
-                child: const SizedBox(
-                  height: 80,
-                  width: 80,
-                  child: Center(
-                    child: Icon(Icons.add),
-                  ),
-                ));
-          }),
+        child: Container(
+          height: 96,
+          width: 96,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(48),
+              color: swatch.SecondaryColors.mediumGray),
+          child: Center(
+            child: Material(
+              elevation: 0,
+              borderRadius: BorderRadius.circular(40),
+              color: swatch.PrimaryColors.blue,
+              child: Builder(builder: (context) {
+                return InkWell(
+                    splashFactory: InkRipple.splashFactory,
+                    borderRadius: BorderRadius.circular(40),
+                    onTap: () {
+                      if (_currentIndex == 0 || _currentIndex == 3) {
+                        showModalBottomSheet(
+                            clipBehavior: Clip.hardEdge,
+                            isScrollControlled: true,
+                            shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(8),
+                                    topRight: Radius.circular(8))),
+                            context: context,
+                            builder: (context) => _sheets[_currentIndex]!);
+                      }
+                    },
+                    child: const SizedBox(
+                      height: 80,
+                      width: 80,
+                      child: Center(
+                        child: Icon(Icons.add),
+                      ),
+                    ));
+              }),
+            ),
+          ),
         ),
       ),
       floatingActionButtonLocation:
@@ -110,28 +129,47 @@ class _HomeScreenState extends State<HomeScreen> {
                 verticalPadding: 12,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const CircleAvatar(
-                        radius: 16,
-                        backgroundColor: swatch.SecondaryColors.mediumGray,
-                      ),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      Text(
-                        "Parto Team",
-                        style: TxtTheme.calloutBold
-                            .copyWith(color: swatch.FillColors.primary),
-                      ),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      const Icon(Icons.keyboard_arrow_down_outlined)
-                    ],
-                  ),
-                  const Icon(Icons.search)
+                  _currentIndex != 4
+                      ? Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const CircleAvatar(
+                              radius: 16,
+                              backgroundColor:
+                                  swatch.SecondaryColors.mediumGray,
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            Text(
+                              "Parto Team",
+                              style: TxtTheme.calloutBold
+                                  .copyWith(color: swatch.FillColors.primary),
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            const Icon(Icons.keyboard_arrow_down_outlined)
+                          ],
+                        )
+                      : BtnFlat(
+                          onTap: () {},
+                          child: Text(
+                            "My Profile",
+                            style: TxtTheme.calloutBold.copyWith(
+                                color: swatch.LabelColors.quarternary),
+                          ),
+                        ),
+                  _currentIndex != 4
+                      ? const Icon(Icons.search)
+                      : BtnFlat(
+                          onTap: () {},
+                          child: Text(
+                            "Sign Out",
+                            style: TxtTheme.calloutBold
+                                .copyWith(color: const Color(0xffff4343)),
+                          ),
+                        )
                 ]),
             const SizedBox(
               height: 16,
--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:taskman/screens/theme/textheme.dart';
+import 'theme/colortheme.dart' as swatch;
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -6,10 +8,136 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
       width: double.infinity,
-      child: Center(
-        child: Text("Profile Page"),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const CircleAvatar(
+                radius: 32,
+                backgroundColor: swatch.SecondaryColors.mediumGray,
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Shoo Phar Nha",
+                    style: TxtTheme.titleSmall
+                        .copyWith(color: swatch.LabelColors.quarternary),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    "xDesign Team",
+                    style: TxtTheme.calloutRegular
+                        .copyWith(color: swatch.FillColors.secondary),
+                  )
+                ],
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Expanded(
+              child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Material(
+              color: swatch.SecondaryColors.mediumGray,
+              borderRadius: BorderRadius.circular(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Text(
+                      "You",
+                      style: TxtTheme.calloutBold
+                          .copyWith(color: swatch.LabelColors.quarternary),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const _Button(txt: "My Information"),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const _Button(txt: "Change Password"),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const _Button(txt: "Change Email"),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const _Button(txt: "More Settings..."),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Text(
+                      "Help",
+                      style: TxtTheme.calloutBold
+                          .copyWith(color: swatch.LabelColors.quarternary),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const _Button(txt: "FAQ"),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const _Button(txt: "Support Inbox"),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const _Button(txt: "Licenses"),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const _Button(txt: "Privacy Policy"),
+                ],
+              ),
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+}
+
+class _Button extends StatelessWidget {
+  final String txt;
+  final TextStyle? style;
+  const _Button({Key? key, required this.txt, this.style}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashFactory: InkRipple.splashFactory,
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              txt,
+              style: style ??
+                  TxtTheme.calloutRegular
+                      .copyWith(color: swatch.LabelColors.quarternary),
+            ),
+            const Icon(Icons.arrow_forward_ios_rounded)
+          ],
+        ),
       ),
     );
   }
